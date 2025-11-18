2cd8844bee9ace038fdb3274ccbd77d3d304047f
luisUCM <luisyama@ucm.es>
2025-11-18 19:46:19 +0100

    AHORA_SI_LAST_COMMIT

--- TOC_practica_05/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_tragaperras_behav/obj/xsim_1.c
+++ TOC_practica_05/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_tragaperras_behav/obj/xsim_1.c
@@ -54,11 +54,11 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_60(char*, char *);
-IKI_DLLESPEC extern void execute_61(char*, char *);
+IKI_DLLESPEC extern void execute_58(char*, char *);
+IKI_DLLESPEC extern void execute_59(char*, char *);
 IKI_DLLESPEC extern void execute_23(char*, char *);
-IKI_DLLESPEC extern void execute_24(char*, char *);
-IKI_DLLESPEC extern void execute_25(char*, char *);
+IKI_DLLESPEC extern void execute_47(char*, char *);
+IKI_DLLESPEC extern void execute_48(char*, char *);
 IKI_DLLESPEC extern void execute_49(char*, char *);
 IKI_DLLESPEC extern void execute_50(char*, char *);
 IKI_DLLESPEC extern void execute_51(char*, char *);
@@ -68,26 +68,24 @@ IKI_DLLESPEC extern void execute_54(char*, char *);
 IKI_DLLESPEC extern void execute_55(char*, char *);
 IKI_DLLESPEC extern void execute_56(char*, char *);
 IKI_DLLESPEC extern void execute_57(char*, char *);
-IKI_DLLESPEC extern void execute_58(char*, char *);
-IKI_DLLESPEC extern void execute_59(char*, char *);
+IKI_DLLESPEC extern void execute_37(char*, char *);
+IKI_DLLESPEC extern void execute_38(char*, char *);
 IKI_DLLESPEC extern void execute_39(char*, char *);
 IKI_DLLESPEC extern void execute_40(char*, char *);
-IKI_DLLESPEC extern void execute_41(char*, char *);
 IKI_DLLESPEC extern void execute_42(char*, char *);
-IKI_DLLESPEC extern void execute_44(char*, char *);
-IKI_DLLESPEC extern void execute_45(char*, char *);
+IKI_DLLESPEC extern void execute_43(char*, char *);
 IKI_DLLESPEC extern void transaction_1(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
 IKI_DLLESPEC extern void transaction_10(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void transaction_11(char*, char*, unsigned, unsigned, unsigned);
-funcp funcTab[26] = {(funcp)execute_60, (funcp)execute_61, (funcp)execute_23, (funcp)execute_24, (funcp)execute_25, (funcp)execute_49, (funcp)execute_50, (funcp)execute_51, (funcp)execute_52, (funcp)execute_53, (funcp)execute_54, (funcp)execute_55, (funcp)execute_56, (funcp)execute_57, (funcp)execute_58, (funcp)execute_59, (funcp)execute_39, (funcp)execute_40, (funcp)execute_41, (funcp)execute_42, (funcp)execute_44, (funcp)execute_45, (funcp)transaction_1, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_10, (funcp)transaction_11};
-const int NumRelocateId= 26;
+funcp funcTab[24] = {(funcp)execute_58, (funcp)execute_59, (funcp)execute_23, (funcp)execute_47, (funcp)execute_48, (funcp)execute_49, (funcp)execute_50, (funcp)execute_51, (funcp)execute_52, (funcp)execute_53, (funcp)execute_54, (funcp)execute_55, (funcp)execute_56, (funcp)execute_57, (funcp)execute_37, (funcp)execute_38, (funcp)execute_39, (funcp)execute_40, (funcp)execute_42, (funcp)execute_43, (funcp)transaction_1, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_10, (funcp)transaction_11};
+const int NumRelocateId= 24;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/tb_tragaperras_behav/xsim.reloc",  (void **)funcTab, 26);
-	iki_vhdl_file_variable_register(dp + 8616);
-	iki_vhdl_file_variable_register(dp + 8672);
+	iki_relocate(dp, "xsim.dir/tb_tragaperras_behav/xsim.reloc",  (void **)funcTab, 24);
+	iki_vhdl_file_variable_register(dp + 7688);
+	iki_vhdl_file_variable_register(dp + 7744);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
--- TOC_practica_05/project_1/project_1.srcs/sources_1/new/tragaperrasASM.vhd
+++ TOC_practica_05/project_1/project_1.srcs/sources_1/new/tragaperrasASM.vhd
@@ -24,71 +24,64 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity tragaperrasASM is
-    Port ( 
-        rst : in STD_LOGIC;
-        clk : in STD_LOGIC;
-        inicio : in STD_LOGIC;
-        fin : in STD_LOGIC;
-        opCodeleds : out STD_LOGIC_VECTOR (2 downto 0);
-        ruleta01 : out STD_LOGIC_VECTOR (3 downto 0);
-        ruleta02 : out STD_LOGIC_VECTOR (3 downto 0)
+    port ( 
+        rst        : in  std_logic;
+        clk        : in  std_logic;
+        inicio     : in  std_logic;
+        fin        : in  std_logic;
+        opcodeleds : out std_logic_vector (2 downto 0);
+        ruleta01   : out std_logic_vector (3 downto 0);
+        ruleta02   : out std_logic_vector (3 downto 0)
     );
 end tragaperrasASM;
 
-architecture Behavioral of tragaperrasASM is
+architecture behavioral of tragaperrasASM is
 
     component divisor
         port (
-            rst: in STD_LOGIC;
-            clk: in STD_LOGIC;
-            cuenta_medio_segundo: out STD_LOGIC;
-            cuenta_display1: out STD_LOGIC;
-            cuenta_display2: out STD_LOGIC
+            rst                 : in  std_logic;
+            clk                 : in  std_logic;
+            cuenta_medio_segundo: out std_logic;
+            cuenta_display1     : out std_logic;
+            cuenta_display2     : out std_logic
         );
     end component;
     
     component contador_mod10
-        port ( 
-           rst : in STD_LOGIC;
-           clk : in STD_LOGIC;
-           enable : in STD_LOGIC;
-           salida : out STD_LOGIC_VECTOR (3 downto 0)
+        port (
+            rst    : in  std_logic;
+            clk    : in  std_logic;
+            enable : in  std_logic;
+            salida : out std_logic_vector (3 downto 0)
         );
-     end component;
-     
+    end component;
+
     type states is (estado_reset, estado_contando, estado_resultado);
     signal estado_actual, estado_siguiente : states;
     
-    signal clk_50ms, clk_fast1, clk_fast2 : STD_LOGIC;
-    signal en_cont1, en_cont2 : STD_LOGIC;
-    signal ruleta01_i, ruleta02_i : STD_LOGIC_VECTOR (3 downto 0);
-    signal ruletas_match : STD_LOGIC;
-
+    signal clk_50ms, clk_fast1, clk_fast2 : std_logic;
+    
+    signal en_cont1, en_cont2 : std_logic;
+    
+    signal ruleta01_i, ruleta02_i : std_logic_vector (3 downto 0);
+    signal ruletas_match : std_logic;
+    
     signal timer_5s_count : unsigned(3 downto 0) := (others => '0');
-    signal timer_5s_en : STD_LOGIC;
-    signal timer_5s_end : STD_LOGIC;
-
-    signal inicio_ff1, inicio_ff2 : std_logic := '0';
-    signal fin_ff1, fin_ff2 : std_logic := '0';
-
-    signal inicio_pulse : std_logic;
-    signal fin_pulse : std_logic;
+    signal timer_5s_en : std_logic;
+    signal timer_5s_end : std_logic;
+    
+    signal inicio_sync : std_logic;
+    signal fin_sync : std_logic;
 
 begin
 
     process(clk)
     begin
         if rising_edge(clk) then
-            inicio_ff1 <= inicio;
-            inicio_ff2 <= inicio_ff1;
-
-            fin_ff1 <= fin;
-            fin_ff2 <= fin_ff1;
+            inicio_sync <= inicio;
+            fin_sync <= fin;
         end if;
     end process;
-
-    inicio_pulse <= '1' when (inicio_ff1 = '1' and inicio_ff2 = '0') else '0';
-    fin_pulse    <= '1' when (fin_ff1 = '1' and fin_ff2 = '0') else '0';
     
     divisor_i: divisor
         port map (
@@ -117,15 +110,15 @@ begin
 
     ruleta01 <= ruleta01_i;
     ruleta02 <= ruleta02_i;
-
+    
     ruletas_match <= '1' when (ruleta01_i = ruleta02_i) else '0';
-
-    opCodeleds <= "100" when estado_actual = estado_reset else
+    
+    opcodeleds <= "100" when estado_actual = estado_reset else
                   "000" when estado_actual = estado_contando else
                   "010" when (estado_actual = estado_resultado and ruletas_match = '1') else
                   "011" when (estado_actual = estado_resultado and ruletas_match = '0') else
                   "000";
-                  
+                      
     en_cont1 <= '1' when estado_actual = estado_contando else '0';
     en_cont2 <= '1' when estado_actual = estado_contando else '0';
     
@@ -147,9 +140,9 @@ begin
             end if;
         end if;
     end process;
-
+    
     timer_5s_end <= '1' when (timer_5s_count = 9 and timer_5s_en = '1' and clk_50ms = '1') else '0';
-
+    
     process(rst, clk)
     begin
         if (rst = '1') then
@@ -159,19 +152,19 @@ begin
         end if;
     end process;
 
-    process(estado_actual, inicio_pulse, fin_pulse, timer_5s_end)
+    process(estado_actual, inicio_sync, fin_sync, timer_5s_end)
     begin
         estado_siguiente <= estado_actual;
 
         case estado_actual is
             
             when estado_reset =>
-                if (inicio_pulse = '1') then 
+                if (inicio_sync = '1') then 
                     estado_siguiente <= estado_contando;
                 end if;
 
             when estado_contando =>
-                if (fin_pulse = '1') then 
+                if (fin_sync = '1') then 
                     estado_siguiente <= estado_resultado;
                 end if;
             
